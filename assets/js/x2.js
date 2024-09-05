06dc52221dd2eb29771afafea196f87f39ef8774
Elie <enkaouae@gmail.com>
2024-09-05 14:27:16 +0200

    Ajout SWOT, Orientations Stratégiques

---
+++ assets/js/x2.js
@@ -0,0 +1,26 @@
+/* Fichier pour donner le resultat d'un cryptage et d'un decryptage */
+
+let keyvalue = ""
+
+function encrypted(decrypted, secretKey) {
+    try {
+        return CryptoJS.AES.encrypt(decrypted, secretKey).toString();
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+function decrypted(encrypt, secretKey) {
+    const decrypted = CryptoJS.AES.decrypt(encrypt, secretKey);
+    const decryptedText = decrypted.toString(CryptoJS.enc.Utf8);
+    return decryptedText
+}
+
+let listeTexte = [
+    
+]
+
+/* for (i of listeTexte){
+    console.log(i, encrypted(i, keyvalue))
+} */
